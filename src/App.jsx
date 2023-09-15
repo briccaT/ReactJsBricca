a3c06ca4e477b09efb017034a75d157f47fef4b7
Tomas <tomibricca1234@gmail.com>
2023-09-14 21:54:17 -0300

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ItemsListContainer from './components/ItemsListContainer'
+import NavBar from './components/NavBar'
+
+
+function App() {
+  return <div>
+    <NavBar />
+    <ItemsListContainer greeting= "HOLA BIENVENIDOS"/>
+</div>
+}
+
+export default App
---
+++ src/components/ItemsListContainer.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+
+
+const ItemsListContainer = ({greeting}) =>  {
+return (
+    <div className="container">
+        {greeting}
+    </div>
+)
+}
+
+export default ItemsListContainer
